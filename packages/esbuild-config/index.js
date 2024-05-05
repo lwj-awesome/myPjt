81971a9038e292e43677af4cd9dc0952f9a886d3
lwj <lwj1267@naver.com>
2024-05-05 18:35:50 +0900

    d?

---
+++ packages/esbuild-config/index.js
@@ -0,0 +1,43 @@
+const { build } = require("esbuild");
+
+const run = ({ entryPoints = ["src/index.ts"], pkg, config = {} }) => {
+  const dev = process.argv.includes("--dev");
+  const minify = !dev;
+  const watch = process.argv.includes("--watch");
+
+  const external = Object.keys({
+    ...pkg.devDependencies,
+    ...pkg.peerdependencies,
+  });
+
+  const baseConfig = {
+    entryPoints,
+    bundle: true,
+    minify,
+    sourcemap: true,
+    outdir: "dist",
+    target: "es2019",
+    watch,
+    external,
+    ...config,
+  };
+
+  Promise.all([
+    build({
+      ...baseConfig,
+      format: "esm",
+    }),
+    build({
+      ...baseConfig,
+      format: "cjs",
+      outExtension: {
+        ".js": ".cjs",
+      },
+    }),
+  ]).catch(() => {
+    console.error("build error");
+    process.exit(1);
+  });
+};
+
+module.exports = run;
--- packages/themes/build.config.js
+++ packages/themes/build.config.js
@@ -1,38 +1,6 @@
-import esbuild from "esbuild";
+import run from "@mypjt/esbuild-config";
 import pkg from "./package.json" assert { type: "json" };
-const dev = process.argv.includes("--dev");
-const minify = !dev;
-const watch = process.argv.includes("--watch");
 
-const external = Object.keys({
-    ...pkg.devDependencies,
-    ...pkg.peerdependencies,
-});
-
-const baseConfig = {
-    entryPoints: ["src/index.ts"],
-    bundle: true,
-    minify,
-    sourcemap: true,
-    outdir: "dist",
-    target: "es2019",
-    watch,
-    external,
-};
-
-Promise.all([
-    esbuild.build({
-        ...baseConfig,
-        format: "esm",
-    }),
-    esbuild.build({
-        ...baseConfig,
-        format: "cjs",
-        outExtension: {
-            ".js": ".cjs",
-        },
-    }),
-]).catch(() => {
-    console.error("build error");
-    process.exit(1);
+run({
+  pkg,
 });
